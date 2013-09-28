06f9f03a3938b13041db7ed3d038a311b29bea41
Mark Strefford <mark.strefford@gmail.com>
2013-09-28 21:36:45 +0100

    Retrieves rate plans based on query string date

--- server.js
+++ server.js
@@ -6,19 +6,46 @@
  * To change this template use File | Settings | File Templates.
  */
 
-var express = require('express');
-var app = express()
+var express = require('express')
     , http = require('http')
     , path = require('path')
     , MongoClient = require('mongodb').MongoClient
     , Server = require('mongodb').Server;
 
-app.set('port', process.env.PORT || 3002);
+var routes = require('./routes')
+var rateplan = require('./routes/rateplan.js');
 
-app.get('/hello.txt', function(req, res){
-    res.send('Hello World');
+var app = express()
+app.set('port', process.env.PORT || 3002);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Dev / prod config options
+
+app.configure('development', function () {
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
+app.configure('production', function () {
+    app.use(express.errorHandler());
+});
+
+// Set up providers
+var ratePlanProvider = new RatePlanProvider('mongodb.local', 'rateplans', 27017);
+
+// Set up routes
+app.get('/', routes.index);
+var ratePlanRoutes = rateplan.addRatePlanRoutes(app, ratePlanProvider);
+
+// Now create the server
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 
